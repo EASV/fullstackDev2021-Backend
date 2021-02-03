3615d65eb6c71c051860cfbbcd973b6379fbcd6c
Lars Juul <ljb@easv.dk>
2021-02-03 11:46:24 +0100

    Adding the information from todays lessons

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,13 +1,20 @@
 import {
-  MessageBody, OnGatewayConnection, OnGatewayDisconnect,
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
+  // Id, Name
+  userMap: Map<string, string> = new Map<string, string>();
+
   @SubscribeMessage('message')
   handleChatEvent(@MessageBody() data: string): string {
     console.log(data);
@@ -15,11 +22,25 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     return data + ' Hello';
   }
 
+  @SubscribeMessage('name')
+  handleNameEvent(
+    @MessageBody() name: string,
+    @ConnectedSocket() client: Socket,
+  ): string {
+    this.userMap.set(client.id, name);
+
+    this.server.emit('clients', Array.from(this.userMap.values()));
+    console.log('map: ', Array.from(this.userMap.values()));
+    return name + ' Hello';
+  }
+
   handleConnection(client: any, ...args: any[]): any {
     console.log('Client Connect', client.id);
   }
 
   handleDisconnect(client: any): any {
+    this.userMap.delete(client.id);
+    this.server.emit('clients', this.userMap.values());
     console.log('Client Disconnect', client.id);
   }
 }
